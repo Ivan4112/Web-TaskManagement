8a4c1c1ce96e7bcb54aea31143f4fde5cef3a1e5
Ivan4112 <ivanbondar224@gmail.com>
2024-02-23 22:27:03 +0200

    Initial commit

---
+++ src/main/java/fpm/dnu/org/WebTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package fpm.dnu.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTaskManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTaskManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fpm/dnu/org/WebTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fpm.dnu.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
